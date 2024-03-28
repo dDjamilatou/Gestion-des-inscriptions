6498ce9bb2dda967fffa76d5c5a2164187daa277
Djamilatou <ddjamilatou998@gmail.com>
2024-03-28 17:42:53 +0000

    Faire Authentification-V1

--- Models/users.model.php
+++ Models/users.model.php
@@ -91,6 +91,7 @@ function FindAllEtudiant(){
         foreach($users as $user){
                 if($user["login"]==$login && $user["mdp"]==$mdp){
                         return $user;
+                        
                 }
         }
         return null;
